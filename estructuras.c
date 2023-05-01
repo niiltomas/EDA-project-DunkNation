f35d3b3e617ce70caba98ed551392c5d5a087219
danial-im <94740373+danial-im@users.noreply.github.com>
2023-05-01 19:30:03 +0200

    Update estructuras.c

--- estructuras.c
+++ estructuras.c
@@ -1,1 +1,10 @@
 //escribe aqui la estructura 
+#define MAX_PREFERENCES
+
+typedef struct {
+    char username[50];
+    int age;
+    char email;
+    char city[50];
+    char preferences[MAX_PREFERENCES][50]
+}User;

e91d22159c4993966cc862cb77d15d9b36480760
Anshpreet197 <129055625+Anshpreet197@users.noreply.github.com>
2023-05-03 09:54:59 +0200

    Update codigo primcipal.h

--- codigo primcipal.h
+++ codigo primcipal.h
@@ -3,5 +3,5 @@
 #define RED_CODIGO_PRIMCIPAL_H
 
 #endif //RED_CODIGO_PRIMCIPAL_H
-
+// funcion menu principal que se encarga de mostrar las opciones del menu y de llamar a las funciones correspondientes a cada opcion
 void menu();

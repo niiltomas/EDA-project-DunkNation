cd47875a0949b54de6a7780c799ca090160dd29c
Nil <nil.tomas01@estudiant.upf.edu>
2023-05-07 22:08:15 +0200

    INTERFAZ practicamente terminada 7/5 22:08

--- library.c
+++ library.c
@@ -8,29 +8,23 @@
 #include "codigo primcipal.c"
 
 
-#define FILE_MENU_NEW 1
-#define FILE_MENU_OPEN 2
 #define FILE_MENU_EXIT 3
-///#define CHANGE_TITLE 4
 #define GENERATE_BUTTON 5
 #define LOGIN 6
-
-///int main() {
-///menu();
-///return 0;
-///}
+#define NEW_PLAYER 7
+#define MAX_PREFERENCES 2
 
 LRESULT CALLBACK WindowProcedure(HWND,UINT,WPARAM,LPARAM);
 
-
+///para incluir una función en el codigo antes se tiene que llamar aqui
 void AddMenus(HWND);
 void AddControls(HWND);
-void registerDialogClass(HINSTANCE);
-void displayDialog(HWND);
 
 
 HMENU hMenu;
 
+///no tocar nada de aqui, ya que es la configuracion de la ventana principal
+/// : ##################################################################################################
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst,LPSTR arg , int ncmdshow)
 {
     WNDCLASSW wc={0};
@@ -42,9 +36,8 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst,LPSTR arg , int ncmdshow
     if(!RegisterClassW(&wc)) {
         return -1;
     }
-    registerDialogClass(hInst);
-    CreateWindowW(L"myWindowClass",L"DUNK NATION",WS_OVERLAPPEDWINDOW | WS_VISIBLE, 300, 100,500,500,
-                  NULL,NULL,NULL,NULL);
+
+    CreateWindowW(L"myWindowClass",L"DUNK NATION",WS_OVERLAPPEDWINDOW | WS_VISIBLE, 300, 100,500,500,NULL,NULL,NULL,NULL);
 
     MSG msg = {0};
     while(GetMessage(&msg,NULL,0,0))
@@ -54,9 +47,12 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst,LPSTR arg , int ncmdshow
     }
     return 0;
 }
+///#########################################################################
 LRESULT CALLBACK WindowProcedure(HWND hwnd,UINT msg,WPARAM wp,LPARAM lp)
 {
-    int aux;
+    User* user= malloc(sizeof(User));
+    char username[20];
+    int aux,password;
     switch(msg)
     {
         case WM_COMMAND: ///ÉS EL MISSATGE
@@ -69,20 +65,38 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd,UINT msg,WPARAM wp,LPARAM lp)
                         DestroyWindow(hwnd);
                     }
                     break;
-                case GENERATE_BUTTON:
+                case GENERATE_BUTTON:///sitio donde se tendrá que poner el codigo de mostrar todos los usuarios
                     break;
-                case LOGIN:
-                    displayDialog(hwnd);
+                case NEW_PLAYER:///en esta parte hay donde se escanea la parte del new player (la entrada es por consola)
+                    MessageBox(hwnd, "click aceptar, then enter name age email and city(separated by space)", "New player", MB_OK);
+                    scanf("%s %d %s %s", user->username, &user->age, user->email, user->city);
+                    MessageBox(hwnd, "click aceptar, then enter preferences separated by spaces", "New player", MB_OK);
+                    for (int i = 0; i < MAX_PREFERENCES; i++) {
+                        scanf("%s", user->preferences[i]);
+                    }
+                    ///prints para comprobar
+                    printf("Username: %s\n", user->username);
+                    printf("Age: %d\n", user->age);
+                    printf("Email: %s\n", user->email);
+                    printf("City: %s\n", user->city);
+                    for (int i = 0; i < MAX_PREFERENCES; i++) {
+                        printf("- %s\n", user->preferences[i]);
+                    }
+                    break;
+                case LOGIN: ///parte donde se escanea los parametros del user
+                    MessageBox(hwnd, "click aceptar, then enter username and a numerical password(separated by space)", "login", MB_OK);
+                    scanf("%s %d",username,&password);
+                    printf("\nuser: %s\n",username);
+                    printf("\npassword: %d\n",password);
                     break;
             }
 
             break;
         case WM_CREATE: ///moment en el què la finestra s'ha creat i tot seguit hi afegim els menus etc. a la finestra
-
             AddMenus(hwnd);
             AddControls(hwnd);
             break;
-        case WM_DESTROY:
+        case WM_DESTROY: ///cierra la ventana cuando se llama a este comando
             PostQuitMessage(0);
             break;
         default:
@@ -92,70 +106,28 @@ LRESULT CALLBACK WindowProcedure(HWND hwnd,UINT msg,WPARAM wp,LPARAM lp)
 
 }
 
-void AddMenus(HWND hwnd)
+void AddMenus(HWND hwnd)///exit de arriba a la izquierda
 {
     hMenu = CreateMenu();
-
     AppendMenu(hMenu,MF_STRING,FILE_MENU_EXIT,"Exit");
-
     SetMenu(hwnd,hMenu);
 
 }
 
 void AddControls(HWND hwnd)
 {
-    ///botons del main
+    ///botones del main
     CreateWindowW(L"Button",L"LOGIN",WS_VISIBLE |WS_CHILD|WS_BORDER,100,50,98,38,hwnd,(HMENU)LOGIN,0,0);
-    CreateWindowW(L"Button",L"NEW PLAYER",WS_VISIBLE |WS_CHILD|WS_BORDER ,250,50,98,38,hwnd,(HMENU)GENERATE_BUTTON,0,0);
+    CreateWindowW(L"Button",L"NEW PLAYER",WS_VISIBLE |WS_CHILD|WS_BORDER ,250,50,98,38,hwnd,(HMENU)NEW_PLAYER,0,0);
     CreateWindowW(L"Button",L"ALL PLAYERS",WS_VISIBLE |WS_CHILD|WS_BORDER,100,120,98,38,hwnd,(HMENU)GENERATE_BUTTON,0,0);
     CreateWindowW(L"Button",L"EXIT",WS_VISIBLE |WS_CHILD|WS_BORDER,250,120,98,38,hwnd,(HMENU)FILE_MENU_EXIT,0,0);
 
-    ///boton login
 }
 
-LRESULT CALLBACK DialogProcedure(HWND hwnd,UINT msg, WPARAM wp, LPARAM lp)
-{
-    switch(msg)
-    {
-        case WM_COMMAND:
-            switch(wp)
-            {
-                case 1:
-                    DestroyWindow(hwnd);
-                    break;
-            }
-            break;
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-            break;
-        default:
-            return DefWindowProcW(hwnd,msg,wp,lp);
 
-    }
-}
 
-void registerDialogClass(HINSTANCE hInst)
-{
-    WNDCLASSW dialog={0};
-    dialog.hbrBackground= (HBRUSH) COLOR_WINDOW;
-    dialog.hCursor = LoadCursor (NULL,IDC_ARROW);
-    dialog.hInstance=hInst;
-    dialog.lpszClassName = L"login";
-    dialog.lpfnWndProc = DialogProcedure;
-
-    RegisterClassW(&dialog);
-
-}
-void displayDialog(HWND hwnd)
-{
-    ///ventana emergente login
-    HWND hDlg= CreateWindowW(L"login",L"LOGIN",WS_VISIBLE | WS_OVERLAPPEDWINDOW,400,400,500,300,hwnd,0,0,0);
-    ///botón cerrar
-    CreateWindowW(L"Button",L"Close",WS_VISIBLE|WS_CHILD,190,200,100,40,hDlg,(HMENU)1,NULL,NULL);
-    ///cuadro de texto: "introduce el nombre de usuario"
-    CreateWindowW(L"Static",L"Introduce el nombre de usuario:",WS_VISIBLE | WS_CHILD |WS_BORDER,20,20, 300,30,hDlg,(HMENU)1,NULL,NULL);/// posicion (x,y) en la ventana (en pixeles) i dimensiones (x,y)
-    CreateWindowW(L"Edit",L"...",WS_VISIBLE |WS_CHILD |WS_BORDER|ES_MULTILINE|ES_AUTOVSCROLL |ES_AUTOHSCROLL,20,70,100,50,hDlg,(HMENU)GENERATE_BUTTON,0,0);
-    ///cuadro de texto: "introduce la contraseña"
-    CreateWindowW(L"Static",L"Introduce la contraseña del usuario:",WS_VISIBLE | WS_CHILD |WS_BORDER,20,130, 300,30,hDlg,(HMENU)1,NULL,NULL);
-    CreateWindowW(L"Edit",L"",WS_VISIBLE |WS_CHILD |WS_BORDER|ES_MULTILINE|ES_AUTOVSCROLL |ES_AUTOHSCROLL,20,170,100,50,hDlg,(HMENU)GENERATE_BUTTON,0,0);
-}
+///otros para mas adelante si tenemos que guardar los valores del user
+// Copy user preferences to User structure
+///for (int i = 0; i < MAX_PREFERENCES; i++) {
+///strncpy(user->preferences[i], preferences[i], sizeof(user->preferences[i]) - 1);
+///user->preferences[i][sizeof(user->preferences[i]) - 1] = '\0';
